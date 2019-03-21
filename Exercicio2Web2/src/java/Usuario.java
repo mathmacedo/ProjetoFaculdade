ecedb0a8940b58a530951f35fe289dad05d74fa9
macedo <matheus.macedo1977@gmail.com>
2019-03-21 10:17:44 -0300

    Corrigindo nomes de projetos e pastas

---
+++ Exercicio2Web2/src/java/Usuario.java
@@ -0,0 +1,52 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author macedo
+ */
+public class Usuario {
+    private String nome;
+    private String login;
+    private String senha;
+    
+    public Usuario() {
+        
+    }
+    
+    public Usuario(String nome, String login, String senha) {
+        this.setNome(nome);
+        this.setLogin(login);
+        this.setSenha(senha);
+    }
+    
+    private String setNome(String nome) {
+        this.nome = nome;
+        return this.nome;
+    }
+    
+    public String getNome() {
+        return this.nome;
+    }
+    
+    private String setLogin(String login) {
+        this.login = login;
+        return this.login;
+    }
+    
+    public String getLogin() {
+        return this.login;
+    }
+    
+    private String setSenha(String senha) {
+        this.senha = senha;
+        return this.senha;
+    }
+    
+    public String getSenha() {
+        return this.senha;
+    }
+}
