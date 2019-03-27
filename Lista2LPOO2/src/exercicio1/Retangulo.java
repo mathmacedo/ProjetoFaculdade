fb57cca5aadd8b4207988e3ffcc6a96729f4c08a
Matheus Macedo <matheus.macedo1977@gmail.com>
2019-03-27 07:48:29 -0300

    Ajustes exerxicios web2, e exs e lista nova de LPOO2

---
+++ Lista2LPOO2/src/exercicio1/Retangulo.java
@@ -0,0 +1,41 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package exercicio1;
+
+/**
+ *
+ * @author macedo
+ */
+public class Retangulo {
+	private double lado1;
+	private double lado2;
+	
+	public Retangulo() {
+		
+	}
+	
+	public Retangulo(double lado1, double lado2) throws RuntimeException {
+		if (lado1 < 0 || lado2 < 0) {
+			throw new RuntimeException("Números negativos não permitidos");
+		}
+		this.lado1 = lado1;
+		this.lado2 = lado2;
+	}
+	
+	public double setLado1(double lado1) {
+		this.lado1 = lado1;
+		return this.lado1;
+	}
+	
+	public double getLado1() {
+		return this.lado1;
+	}
+	
+	public double setLado2(double lado2) {
+		this.lado2 = lado2;
+		return this.lado2;
+	}
+}
---
+++ Lista2LPOO2/src/exercicio1/Triangulo.java
@@ -0,0 +1,45 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package exercicio1;
+
+/**
+ *
+ * @author macedo
+ */
+public class Triangulo {
+	private double base; 
+	private double altura; 
+	
+	public Triangulo() {
+		
+	}
+	
+	public Triangulo(double base, double altura) throws RuntimeException {
+		if(base < 0 || altura < 0) {
+			throw new RuntimeException("Números negativos não permitidos");
+		}
+		this.base = base;
+		this.altura = altura;
+	}
+	
+	public double setBase(double base) {
+		this.base = base;
+		return this.base;
+	}
+	
+	public double getBase() {
+		return this.base;
+	}
+	
+	public double setAltura(double altura) {
+		this.altura = altura;
+		return this.altura;
+	}
+	
+	public double getAltura() {
+		return this.altura;
+	}
+}
