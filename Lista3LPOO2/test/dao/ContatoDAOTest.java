5bb746a7ef2f8324b5ec69f4b213ae9bb9756fdf
macedo <matheus@lilibox.com.br>
2019-04-08 13:31:13 -0300

    Feito classes de teste para ex1 LISTA3 LPOO2

---
+++ Lista3LPOO2/test/dao/ContatoDAOTest.java
@@ -0,0 +1,98 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package dao;
+
+import java.util.Calendar;
+import java.util.GregorianCalendar;
+import java.util.List;
+import model.Contato;
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ *
+ * @author macedo
+ */
+public class ContatoDAOTest {
+    
+    @Test
+    public void testAlteraContato() {
+        ContatoDAO novoContatoDAO = new ContatoDAO();
+        Contato novoContato = new Contato();
+        Calendar calendar = new GregorianCalendar(2018, 10, 28);
+        novoContato.setDataNascimento(calendar);
+        novoContato.setId(6);
+        novoContatoDAO.altera(novoContato);
+    }
+    
+    @Test
+    public void testAlteraContatoNaoExistente() {
+        try {
+            ContatoDAO novoContatoDAO = new ContatoDAO();
+            Contato novoContato = new Contato();
+            Calendar calendar = new GregorianCalendar(2018, 10, 28);
+            novoContato.setId(100);
+            novoContatoDAO.altera(novoContato);
+            fail("Teste deu Errado");
+        } catch (RuntimeException e) {
+            assertEquals("Contato não encontrado", e.getMessage());
+        }
+    }
+ 
+    @Test
+    public void testRemoveContato() {
+        ContatoDAO novoContatoDAO = new ContatoDAO();
+        Contato novoContato = new Contato();
+        novoContato.setId(3);
+        novoContatoDAO.remove(novoContato);
+    }
+    
+    @Test
+    public void testRemoveContatoNaoExistente() {
+        try {
+            ContatoDAO novoContatoDAO = new ContatoDAO();
+            Contato novoContato = new Contato();
+            novoContato.setId(3);
+            novoContatoDAO.remove(novoContato);
+            fail("Teste deu Errado");
+        } catch (RuntimeException e) {
+            assertEquals("Contato não encontrado", e.getMessage());
+        }
+    }
+    
+    @Test
+    public void testInsereContato() {
+        ContatoDAO novoContatoDAO = new ContatoDAO();
+        Contato novoContato = new Contato();
+        Calendar calendar = new GregorianCalendar(2018, 10, 28);
+        novoContato.setNome("Matheus");
+        novoContato.setEmail("teste@teste.com");
+        novoContato.setEndereco("teste 123");
+        novoContato.setEndereco("teste 123");
+        novoContato.setDataNascimento(calendar);
+    }
+    
+    @Test
+    public void testInsereContatoErrado() {
+        try {
+            ContatoDAO novoContatoDAO = new ContatoDAO();
+            Contato novoContato = new Contato();
+            Calendar calendar = new GregorianCalendar(2018, 10, 28);
+            novoContato.setNome("");
+            novoContato.setEmail("teste@teste.com");
+            novoContato.setEndereco("teste 123");
+            novoContato.setEndereco("teste 123");
+            novoContato.setDataNascimento(calendar);
+            fail("Teste deu Errado");
+        } catch (RuntimeException e) {
+            assertEquals("Nome não pode ser vazio!", e.getMessage());
+        }
+    }
+}
--- Lista3LPOO2/test/model/ContatoTest.java
+++ Lista3LPOO2/test/model/ContatoTest.java
@@ -5,7 +5,6 @@
  */
 package model;
 
-import java.util.Calendar;
 import org.junit.Test;
 import static org.junit.Assert.*;
 
